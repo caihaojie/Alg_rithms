36a456e8e8fbb051cbf63157eaf8e69786082fed
caihaojie <u201413380@hust.edu.cn>
2016-04-22 23:55:45 +0800

    finished

--- 插入排序.c
+++ 插入排序.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 
+void quick_sort();
+
 int main()
 {
-	int tar[6] = {5,4,6,7,9,0};
-	int i = 0, j = 0, key = 0;
-	for (j = 1; j <= 5; j++)
+	int tar[6] = { 5,4,6,7,9,0 };
+	quick_sort(tar);
+
+	//show the result
+	for (int i = 1; i < 6; i++)
 	{
-		key = tar[j];
+		printf("%d\n", tar[i]);
+	}
+	return 0;
+}
+
+/*************************************************
+Function: quick_sort(int* head)
+Description: 对数组元素进行排序
+Calls: NULL
+Called By: main()
+Input:  int* head->数组首地址
+Output: NULL
+Return: NULL
+Others: head[0] = head.size,head[0]不会参与排序
+*************************************************/
+void quick_sort(int* head)
+{
+	int key, i;
+
+	for (int j = 1; j <= head[0]; j++)
+	{
+		key = head[j];
 		i = j - 1;
-		while (i >=0  && key<tar[i])
+
+		while (i >= 1 && key<head[i])
 		{
-			tar[i + 1] = tar[i];
-			tar[i] = key;
+			head[i + 1] = head[i];
+			head[i] = key;
 			i--;
 		}
 	}
-	for (i = 0; i < 6; i++)
-		printf("%d\n",tar[i]);
-	return 0;
 }
